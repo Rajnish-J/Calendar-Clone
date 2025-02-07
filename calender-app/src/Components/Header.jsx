ce4045d4fe3014ef13eb3da862862258201c8972
Rajnish-J <rajnishofficial02@gmail.com>
2025-02-07 22:04:08 +0530

    feat: header name changed in styling and setup for deployment

--- calender-app/src/Components/Header.jsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-blue-500 text-white py-4 px-6">
-      <div className="container mx-auto">
-        <h1 className="text-2xl font-bold">CalendarMate</h1>
-      </div>
-    </header>
-  );
-}
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="bg-purple-600 text-white py-4 px-6">
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-bold">
+          <span className="text-stone-950 mr-0.5">C</span>alendar
+          <span className="text-stone-950 ml-0.5">M</span>ate
+        </h1>
+      </div>
+    </header>
+  );
+}
